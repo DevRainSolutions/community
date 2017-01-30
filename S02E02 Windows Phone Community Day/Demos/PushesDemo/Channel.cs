c01defb53ca475b2b9913d4c6c68a559fbf28a51
sashaeve <info@devrain.com>
2017-01-30 22:58:11 +0200

    Added second season.

---
+++ S02E02 Windows Phone Community Day/Demos/PushesDemo/Channel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushesDemo
+{
+    public class Channel
+    {
+        public int Id { get; set; }
+        public string DeviceId { get; set; }
+        public string Uri { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
